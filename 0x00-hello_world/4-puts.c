e16b1924ba568a4f0b977051f9adaae0be3e3595
toniacash7 <anthoniaanyanwu@gmail.com>
2023-02-16 18:58:46 -0800

    task 4 c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main a program that prints a line with put fuction
+ *This is like an algorithm of the task
+ *Return: 0 (success)
+ */
+int main(void)
+{
+puts("\""Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,2 +1,3 @@
 alx-low_level_programming
 gcc -S command to generates the assembly code of a C code and save it in an output file
+task 4 is first c program in alx
