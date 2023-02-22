83015d9dd9f1c3aca7abcfac687e2e8b1e044dfa
toniacash7 <anthoniaanyanwu@gmail.com>
2023-02-22 03:07:09 -0800

    task 12 combination of number debudded

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,21 +8,26 @@
 int main(void)
 {
 int x, y;
-for (x = 0, x < 98; x++)
+
+for (x = 0; x <= 98; x++)
 {
-	for (y = x + 1, y < 99; y++)
+	for (y = x + 1; y <= 99; y++)
 	{
 		putchar((x / 10) + '0');
 		putchar((x % 10) + '0');
 		putchar(' ');
 		putchar((y / 10) + '0');
 		putchar((y % 10) + '0');
+
 		if (x == 98 && y == 99)
 			continue;
+
 		putchar(',');
 		putchar(' ');
 	}
 }
+
 putchar('\n');
+
 return (0);
 }
