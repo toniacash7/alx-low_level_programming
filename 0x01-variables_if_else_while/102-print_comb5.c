49ba6ca77ca256dcf724581c2dea8d553b0e1716
toniacash7 <anthoniaanyanwu@gmail.com>
2023-02-22 02:57:58 -0800

    task 12 two digits  number combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of two digit numbers,
+ * ranging from 0 to 99, seprated by comma.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x, y;
+for (x = 0, x < 98; x++)
+{
+	for (y = x + 1, y < 99; y++)
+	{
+		putchar((x / 10) + '0');
+		putchar((x % 10) + '0');
+		putchar(' ');
+		putchar((y / 10) + '0');
+		putchar((y % 10) + '0');
+		if (x == 98 && y == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
